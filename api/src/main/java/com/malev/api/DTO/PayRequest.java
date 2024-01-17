3429ce4d1d03a126a33d5ea163a399a5b1f4d859
Barney Dono <sovietspy2@users.noreply.github.com>
2024-01-17 07:55:25 +0100

    production fix

--- api/src/main/java/com/malev/api/DTO/PayRequest.java
+++ api/src/main/java/com/malev/api/DTO/PayRequest.java
@@ -1,4 +1,4 @@
 package com.malev.api.DTO;
 
-public record PayRequest(String card, String name) {
+public record PayRequest(String cardNumber, String cardName, String CCV, String currency, String orderNumber) {
 }
