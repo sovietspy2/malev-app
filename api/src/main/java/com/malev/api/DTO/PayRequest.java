e3d23412b9b72c08ed9fc4380a61f8ca4213bfed
Barney Dono <sovietspy2@users.noreply.github.com>
2024-01-17 08:10:24 +0100

    working on payment API

--- api/src/main/java/com/malev/api/DTO/PayRequest.java
+++ api/src/main/java/com/malev/api/DTO/PayRequest.java
@@ -1,4 +1,4 @@
 package com.malev.api.DTO;
 
-public record PayRequest(String card, String name, String currency) {
+public record PayRequest(String card, String name, String currency, String payeeName, Integer amount) {
 }
